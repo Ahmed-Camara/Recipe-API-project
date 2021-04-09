389131950ec1228ff2c0644bb4adfa2496730cef
Ahmed-Camara <camarahmed123@gmail.com>
2021-04-09 09:16:03 +0600

    building search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,38 @@
-//https://recipesapi.herokuapp.com
-
-//https://recipesapi.herokuapp.com/api/search
-
 import Search from './models/Search';
 
-const search = new Search('pizza');
+/**Global state of the app
+ * Search object
+ * Current recipe object
+ * Shopping list object
+ * Liked recipes
+ * 
+ * **/
 
-console.log(search);
+const state = {};
 
-search.getResults();
+const controlSearch = async () => {
+
+
+    //1. get query from view
+    const query = document.querySelector('.search__field').value;
+
+    if(query){
+
+        //2. new search object and add it to state
+        state.search = new Search(query);
+        
+        //3. Prepare UI for results
+        //4. Search for recipes
+        await state.search.getResults();
+
+        //5. result results on UI
+        console.log(state.search.result);
+    }
+};
+
+document.querySelector('.search').addEventListener('submit',e=>{
+
+    e.preventDefault();
+
+    controlSearch();
+});
