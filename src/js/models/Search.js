71a55ff9257c520e89c1d4711af7ed98eb645ac2
Ahmed-Camara <camarahmed123@gmail.com>
2021-04-09 08:58:25 +0600

    Building search model

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,27 @@
-export default 'Iam an exported string';
+import axios from 'axios';
+export default class Search{
+
+    constructor(query){
+
+        this.query = query;
+    }
+
+    async getResults(){
+    
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+        try{
+            const res = await axios(`${proxy}https://recipesapi.herokuapp.com/api/search?q=${this.query}`);
+
+            this.result = res.data.recipes;
+            
+            console.log(this.result);
+        
+        }catch(error){
+
+            alert('Error, something went wrong');
+        }
+    
+        
+    }
+}
