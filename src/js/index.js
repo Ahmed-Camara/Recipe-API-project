338188bf5560341b4458df193340fea8fc67c693
Ahmed-Camara <camarahmed123@gmail.com>
2021-04-12 08:34:30 +0600

    Rendering an AJAX Loading Spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/SearchView';
-import {elements} from './views/base';
+import {elements,renderLoader,clearLoader} from './views/base';
 /**Global state of the app
  * Search object
  * Current recipe object
@@ -25,11 +25,13 @@ const controlSearch = async () => {
         //3. Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         //4. Search for recipes
         await state.search.getResults();
 
         //5. result results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
   }
 }
